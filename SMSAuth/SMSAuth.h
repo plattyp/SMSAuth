5ac1b19d4c159baa3a357de1ee8b3120ed34f403
Andrew Platkin <andrew.platkin@gmail.com>
2017-01-29 19:50:51 -0600

    Initial Commit

---
+++ SMSAuth/SMSAuth.h
@@ -0,0 +1,19 @@
+//
+//  SMSAuth.h
+//  SMSAuth
+//
+//  Created by Andrew Platkin on 1/29/17.
+//  Copyright © 2017 Andrew Platkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMSAuth.
+FOUNDATION_EXPORT double SMSAuthVersionNumber;
+
+//! Project version string for SMSAuth.
+FOUNDATION_EXPORT const unsigned char SMSAuthVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMSAuth/PublicHeader.h>
+
+
---
+++ SMSAuthTests/SMSAuthTests.swift
@@ -0,0 +1,36 @@
+//
+//  SMSAuthTests.swift
+//  SMSAuthTests
+//
+//  Created by Andrew Platkin on 1/29/17.
+//  Copyright © 2017 Andrew Platkin. All rights reserved.
+//
+
+import XCTest
+@testable import SMSAuth
+
+class SMSAuthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
